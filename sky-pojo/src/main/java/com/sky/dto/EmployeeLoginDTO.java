6f42f16a23469e4d0204e13caa76dffd86bdaa03
LuoYeYinZhongXing <anlinwei_2022@qq.com>
2025-11-26 11:38:09 +0800

    使用@Api等相关注解，增加了关于员工登录功能类由knife4j自动生成的api接口文档里的注解

--- sky-pojo/src/main/java/com/sky/dto/EmployeeLoginDTO.java
+++ sky-pojo/src/main/java/com/sky/dto/EmployeeLoginDTO.java
@@ -7,13 +7,13 @@ import lombok.Data;
 import java.io.Serializable;
 
 @Data
-@ApiModel(description = "员工登录时传递的数据模型")
+@ApiModel(description = "员工登录时传递的数据模型")//描述这个DTO
 public class EmployeeLoginDTO implements Serializable {
 
-    @ApiModelProperty("用户名")
+    @ApiModelProperty("用户名")//描述这个属性
     private String username;
 
-    @ApiModelProperty("密码")
+    @ApiModelProperty("密码")//描述这个属性
     private String password;
 
 }
