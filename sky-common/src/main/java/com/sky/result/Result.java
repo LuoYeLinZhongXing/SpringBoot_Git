b33fe555ab5c125256b33ca13340240cd0056d9c
LuoYeYinZhongXing <anlinwei_2022@qq.com>
2025-11-26 13:58:18 +0800

    新增员工的代码开发

--- sky-common/src/main/java/com/sky/result/Result.java
+++ sky-common/src/main/java/com/sky/result/Result.java
@@ -1,5 +1,6 @@
 package com.sky.result;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.Serializable;
--- sky-pojo/src/main/java/com/sky/dto/EmployeeDTO.java
+++ sky-pojo/src/main/java/com/sky/dto/EmployeeDTO.java
@@ -1,5 +1,6 @@
 package com.sky.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -7,16 +8,22 @@ import java.io.Serializable;
 @Data
 public class EmployeeDTO implements Serializable {
 
+    @ApiModelProperty(value = "员工id")
     private Long id;
 
+    @ApiModelProperty(value = "用户名")
     private String username;
 
+    @ApiModelProperty(value = "姓名")
     private String name;
 
+    @ApiModelProperty(value = "手机号")
     private String phone;
 
+    @ApiModelProperty(value = "性别")
     private String sex;
 
+    @ApiModelProperty(value = "身份证号")
     private String idNumber;
 
 }
