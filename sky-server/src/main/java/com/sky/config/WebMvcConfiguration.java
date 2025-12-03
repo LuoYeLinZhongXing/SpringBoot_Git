f4c41aaf08b52e1f1a9ccfec69c0f14cd3418194
LuoYeYinZhongXing <anlinwei_2022@qq.com>
2025-12-03 10:45:39 +0800

    缓存菜品功能代码的开发

--- sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
+++ sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
@@ -46,7 +46,9 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
         registry.addInterceptor(jwtTokenUserInterceptor)
                 .addPathPatterns("/user/**")
                 .excludePathPatterns("/user/user/login")
-                .excludePathPatterns("/user/shop/status");
+                .excludePathPatterns("/user/shop/status")
+                .excludePathPatterns("/user/category/list")
+                .excludePathPatterns("/user/dish/list");
     }
 
     /**
--- sky-server/src/main/java/com/sky/controller/user/DishController.java
+++ sky-server/src/main/java/com/sky/controller/user/DishController.java
@@ -10,12 +10,13 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController("userDishController")
-@RequestMapping("/user/user/dish")
+@RequestMapping("/user/dish")
 @Api(tags = "C端菜品接口")
 @Slf4j
 public class DishController {
@@ -23,15 +24,31 @@ public class DishController {
     @Autowired
     private DishService dishService;
 
+    @Autowired
+    private RedisTemplate redisTemplate;
+
     @GetMapping("/list")
     @ApiOperation("根据分类id查询菜品")
     public Result<List<DishVO>> list( Long categoryId){
         log.info("根据分类id查询菜品：{}",categoryId);
+
+        //查询Redis中是否有缓存的菜品数据
+        String key = "dish_" + categoryId;
+        List<DishVO> List  = (List<DishVO>)redisTemplate.opsForValue().get(key);
+        if(List != null && List.size() > 0){
+            //缓存中有数据，直接返回
+            return Result.success(List);
+        }
+
+
         Dish dish = new Dish();
         dish.setCategoryId(categoryId);
         dish.setStatus(StatusConstant.ENABLE);
 
         List<DishVO> dishVOList= dishService.getListByCategoryIdWithFlavor(dish);
+        //缓存中没有数据，查询数据库,并放在Redis中
+        redisTemplate.opsForValue().set(key,dishVOList);
+
         return Result.success(dishVOList);
     }
 }
